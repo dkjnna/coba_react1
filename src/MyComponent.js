e6010ae8af2d60d9c85e75e564a8317073723f3f
dkjnna <faridfadilah70@gmail.com>
2024-07-28 12:19:16 +0700

    react-2 menambahkan semua

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './MyComponent.css'
+
+function MyComponent() {
+    return(
+        <div className="compo">
+            <h1>Ini dari component</h1>
+        </div>
+    );
+}
+
+export default MyComponent;
